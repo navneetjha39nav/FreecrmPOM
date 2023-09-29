fd1ff335ba343921460d14acc7da5013d6d40464
NAVNEET KUMAR JHA <navneetjha39@gmial.com>
2023-09-29 14:10:28 +0530

    navneet

--- src/main/java/com/crm/pages/Loginpage.java
+++ src/main/java/com/crm/pages/Loginpage.java
@@ -1,7 +1,19 @@
 package com.crm.pages;
 
 import com.crm.baseclass.TestBaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import javax.xml.xpath.XPath;
+import java.io.IOException;
 
 public class Loginpage extends TestBaseClass {
 
+    public Loginpage() throws IOException {
+        @FindBy(XPath="//*[@href=\"https://ui.freecrm.com\"]");
+        WebElement LogIn;
+        LogIn.click();
+
+
+    }
 }
