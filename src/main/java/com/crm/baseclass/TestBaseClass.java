3f93fee756a39d53b63f9043c17caf42519ddbfd
NAVNEET KUMAR JHA <navneetjha39@gmial.com>
2023-09-29 12:06:31 +0530

    navneet

---
+++ src/main/java/com/crm/baseclass/TestBaseClass.java
@@ -0,0 +1,40 @@
+package com.crm.baseclass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+public class TestBaseClass {
+    static WebDriver Driver;
+    static Properties Prop;
+
+    public TestBaseClass() throws IOException {
+
+        Properties Prop = new Properties();
+        FileInputStream Fis = new FileInputStream("C:\\Users\\NAVNEET KUMAR JHA\\IdeaProjects\\FreeCrmTest\\src\\main\\java\\com\\crm\\config\\propities");
+        Prop.load(Fis);
+
+    }
+
+
+    public static void Readfile() {
+        String Browser1 = Prop.getProperty("Browser");
+        if (Browser1.equals("chrome")) {
+            Driver = new ChromeDriver();
+        } else if (Browser1.equals("FirefoxDriver")) {
+            Driver = new FirefoxDriver();
+
+        } else if (Browser1.equals("EdgeDriver")) {
+            Driver = new EdgeDriver();
+
+        } else {
+            System.out.println("plese select proper browser name");
+        }
+
+    }
+}
+
+
+
